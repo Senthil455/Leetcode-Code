c0cc51adfd982d1bdc4d65ae59e86c0e39219e0c
Senthil <senthilrajasen637@gmail.com>
2026-08-01 18:51:12 +0530

    Time: 7 ms (82.29%) | Memory: 71.2 MB (12.84%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.c
@@ -0,0 +1,15 @@
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize){
+    int * ret = calloc(temperaturesSize, sizeof(int));
+    *returnSize = temperaturesSize;
+    int * stk = calloc(temperaturesSize, sizeof(int)); // store indexes
+    int pt = -1; // stack pointer (point to the top element, '-1' represent the stack is currently empty)
+    
+    // starting from the end of the array
+    for (int i = temperaturesSize-1; i >= 0; i--){
+        while(pt != -1 && temperatures[i] >= temperatures[stk[pt]]) pt--; // pop
+        ret[i] = (pt == -1? 0: stk[pt]-i);
+        stk[++pt] = i; // push index to stack
+    }
+    free(stk);
+    return ret;
+}
