b50499f736e36cb33f05eb25d776341e49f70eb5
Senthil <senthilrajasen637@gmail.com>
2026-08-13 22:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (17.41%) - LeetSync

---
+++ 120-triangle/triangle.c
@@ -0,0 +1,15 @@
+int min(int a,int b)
+{
+    return a<b? a: b;
+}
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
+    for(int i= triangleSize-2;i>=0;i--)
+    {
+        for(int j=0;j<triangleColSize[i];j++)
+        {
+            triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1]);
+        }
+    }
+    return triangle[0][0];
+    
+}
