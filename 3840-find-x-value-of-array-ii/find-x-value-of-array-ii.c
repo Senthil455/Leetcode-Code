4816ebcac99361cd4f36a473dc1e0dda928f31bb
Senthil <senthilrajasen637@gmail.com>
2026-09-22 22:45:40 +0530

    Time: 341 ms (0.00%) | Memory: 110.2 MB (0.00%) - LeetSync

--- 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.c
+++ 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Node maintains remainder frequencies and subarray product.
 typedef struct {
     int cntPerRem[5];
     int prod;
 } SegTreeNode;
 
-// Combine left and right subtrees into a single aggregated node.
 void mergeNodes(SegTreeNode* res, const SegTreeNode* l, const SegTreeNode* r, int modVal) {
     memset(res->cntPerRem, 0, sizeof(int) * 5);
     res->prod = (l->prod * r->prod) % modVal;
@@ -21,7 +19,6 @@ void mergeNodes(SegTreeNode* res, const SegTreeNode* l, const SegTreeNode* r, in
     }
 }
 
-// Initialize leaf node with a specific value modulo constraint.
 void addNode(SegTreeNode* tree, int idx, int val, int modVal) {
     val %= modVal;
     memset(tree[idx].cntPerRem, 0, sizeof(int) * 5);
@@ -29,7 +26,6 @@ void addNode(SegTreeNode* tree, int idx, int val, int modVal) {
     tree[idx].prod = val;
 }
 
-// Recursively build segment tree by dividing array range.
 void buildTree(SegTreeNode* tree, const int* nums, int node, int l, int r, int modVal) {
     if (l == r) {
         addNode(tree, node, nums[l], modVal);
@@ -38,11 +34,9 @@ void buildTree(SegTreeNode* tree, const int* nums, int node, int l, int r, int m
     int mid = l + (r - l) / 2;
     buildTree(tree, nums, node * 2, l, mid, modVal);
     buildTree(tree, nums, node * 2 + 1, mid + 1, r, modVal);
-    // Propagate updated states up through the tree structure.
     mergeNodes(&tree[node], &tree[node * 2], &tree[node * 2 + 1], modVal);
 }
 
-// Traverse tree to update target leaf and recalculate ancestors.
 void updateTree(SegTreeNode* tree, int node, int l, int r, int idx, int val, int modVal) {
     if (l == r) {
         addNode(tree, node, val, modVal);
@@ -56,8 +50,6 @@ void updateTree(SegTreeNode* tree, int node, int l, int r, int idx, int val, int
     }
     mergeNodes(&tree[node], &tree[node * 2], &tree[node * 2 + 1], modVal);
 }
-
-// Retrieve merged node representing the queried subarray range.
 SegTreeNode queryTree(SegTreeNode* tree, int node, int l, int r, int ql, int qr, int mod) {
     if (ql <= l && r <= qr) return tree[node];
     
